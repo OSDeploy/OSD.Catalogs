1e96831ce6efac489c53aeac132e6745ced120bb
David Segura <david@segura.org>
2025-04-04 11:19:52 -0500

    Add detailed documentation for Get-OSDCatalogsPath function

--- OSD.Catalogs.psm1
+++ OSD.Catalogs.psm1
@@ -1,4 +1,24 @@
 function Get-OSDCatalogsPath {
+    <#
+    .SYNOPSIS
+        Returns the path to the catalogs directory of the OSD.Catalogs module.
+    
+    .DESCRIPTION
+        This function returns the full file system path to the 'catalogs' directory
+        located within the OSD.Catalogs module.
+    
+    .EXAMPLE
+        PS C:\> $CatalogsPath = Get-OSDCatalogsPath
+        PS C:\> $CatalogsPath
+        C:\Program Files\WindowsPowerShell\Modules\OSD.Catalogs\0.0.1\catalogs
+    
+    .OUTPUTS
+        System.String
+        Returns a string containing the full path to the catalogs directory.
+    
+    .NOTES
+        David Segura
+    #>
     [CmdletBinding()]
     param ()
 
